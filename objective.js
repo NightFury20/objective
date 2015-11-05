667eb010363dc71e475e49d9fb5c65701f5e9510
Barry Doyle <barrydoyle18@gmail.com>
2015-11-05 14:20:55 +0200

    Prototype login done

--- objective.js
+++ objective.js
@@ -1,44 +1,91 @@
-Objectives = new Mongo.Collection("objectives");
+if (Meteor.isClient) {
+	// This code only runs on the client (meant for the interface)
+	// Trim helper
+	var trimInput = function(val) {
+		return val.replace(/^\s*|\s*$/g, "");
+	};
+
+	Template.login.events({	// Responds to login submit event
+ 		'submit #login-form': function(event, t) {	//When form is submitted
+ 			event.preventDefault();
+ 			// Retrieve the input field values
+ 			var email = document.getElementById('loginEmail').value,
+ 				password = document.getElementById('loginPassword').value;
+
+			// Trim and validation your fields here....
+			email = trimInput(email);
+
+			// If validation passes, supply the appropriate fields to the Meteor.loginWithPassword() function.
+			Meteor.loginWithPassword(email, password, function(err) {
+				if (err) {
+					// The user might not have been found, or their password could be incorrect.  Inform the user that their login attempt has failed
+					alert("Login failed");
+					console("Login failed");
+				}
+				else {
+					// The user has been logged in.
+					console.log("Login sucessful");
+				}
+			});
+ 			return false;
+ 		},
+ 		'submit #signup-form': function(event) { // When form is submitted
+ 			event.preventDefault();
+ 			var validated = true,
+ 				email = document.getElementById('signupEmail').value,
+		    	name = document.getElementById('signupDisplayName').value,
+		    	password = document.getElementById('signupPassword').value,
+		    	password2 = document.getElementById('signupConfirmPassword').value;
+
+		    // Trim and validate the input
+			email = trimInput(email);
+			name = trimInput(name);
+
+			if (name === "") {
+				name = null;
+			}
+
+			if (password.length < 6) {
+				validated = false;
+				alert("Password is too short (must be at least 6 characters long");
+				console.log("Password too short (must be at least 6 characters long");
+			}
+
+		    if(password !== password2) {
+		    	validated = false;
+		    	alert("Passwords don't match");
+		    	console.log("Passwords don't match");
+		    }
+
+		    if (validated) {
+			    Accounts.createUser({username: email, email: email, password : password, profile:{name: name}}, function(err){
+			        if (err) {
+			    	    // Inform the user that account creation failed
+			    	    alert("Account creation failed");
+			    	    console.log("Account creation failed");
+			        } else {
+			            // Success. Account has been created and the user has logged in successfully. 
+			            console.log("Account has successfully been created");
+			        }
+			    });
+			} else {
+				// Not validated, inform user
+				console.log("Not validated");
+			}
+		    return false;
+		}
+	});
+
+ 	Template.dashboard.events({
+ 		'click #logout': function(event) {
+ 			Meteor.logout();
+ 		}
+ 	});
+}
 
 if (Meteor.isServer) {
 	// This code only runs on the server
   	Meteor.startup(function () {
     	// code to run on server at startup
   	});
-}
-
-if (Meteor.isClient) {
-	// This code only runs on the client (meant for the interface)
-
- 	Template.signup.events({	// Responds to sign up submit event
- 		'submit form': function(event) {
- 			event.preventDefault(); // No default behaviour
- 			var emailVar = event.target.signupEmail.value; // Store email value from form
- 			var passwordVar = event.target.signupPassword.value; // Store password value from form
- 			console.log("Sign up form submitted."); // Output message in console
-
- 			Accounts.createUser({
- 				email: emailVar,
- 				password: passwordVar
- 			});
- 		}
- 	});
-
- 	Template.login.events({	// Responds to login submit event
- 		'submit form': function(event) {
- 			event.preventDefault();	// No default behaviour
- 			var emailVar = event.target.loginEmail.value;	// Store email value from form
- 			var passwordVar = event.target.loginPassword.value;	// Store password value from form
- 			console.log("Login form submitted")	// Output message in console
-
- 			Meteor.loginWithPassword(emailVar, passwordVar);
- 		}
- 	});
-
- 	Template.dashboard.events({
- 		'click .logout': function(event) {
- 			event.preventDefault();	//	No default behaviour
- 			Meteor.logout();	// Call meteors logout method
- 		}
- 	})
 }
